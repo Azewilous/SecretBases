ce7f0dfa832f1018af1cd872d3dbc964e49f83f2
Azewilous <lucarzard@gmail.com>
2014-08-28 13:51:00 -0400

    Added a structure generator with command "make". Still needs editing though

--- SecretBases/src/me/leaf/secretbases/SecretBases.java
+++ SecretBases/src/me/leaf/secretbases/SecretBases.java
@@ -2,6 +2,14 @@ package me.leaf.secretbases;
 
 import java.util.logging.Logger;
 
+import net.minecraft.server.v1_7_R4.Block;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,5 +23,28 @@ public class SecretBases extends JavaPlugin {
 	
 	   public ChunkGenerator getDefaultWorldGenerator(String worldName, String id){
 		   return new FlatLandGenerator(this);
-	   }
+	  }
+      public boolean onCommand(CommandSender sender, Command cmd, 
+    		  String label, String[] args){
+    	  if(sender instanceof Player){
+    		  Player p = (Player) sender;
+    		  if((label.equalsIgnoreCase("make"))&&(args.length == 0)){
+    			  @SuppressWarnings("deprecation")
+				Block target = (Block) p.getTargetBlock(null,40);
+    			  if(target != null);
+    			  for(int x = 0;x < 16; x++){
+    				  for(int z = 0;z < 16; z++){
+    					  for(int y = 0;y < 16; y++){
+    						  int tx = ((org.bukkit.block.Block) target).getX();
+    						  int ty = ((org.bukkit.block.Block) target).getY();
+    						  int tz = ((org.bukkit.block.Block) target).getZ();
+    						  Location blockLoc = new Location(((Entity) target).getWorld(), tx + x, ty + y, tz + z);
+    						  blockLoc.getBlock().setType(Material.WOOD);
+    					  }
+    				  }
+    			  }
+    		  }
+    	  }
+          return false;
+      }
 }
