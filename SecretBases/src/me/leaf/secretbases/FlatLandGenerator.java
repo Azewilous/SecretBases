50476d15ff29a8071d8dbfb31a7c4a4da0ae72ac
Azewilous <lucarzard@gmail.com>
2014-08-28 12:53:40 -0400

    Changed the byte to a short, due to the World Generation Issue

--- SecretBases/src/me/leaf/secretbases/FlatLandGenerator.java
+++ SecretBases/src/me/leaf/secretbases/FlatLandGenerator.java
@@ -26,33 +26,7 @@ public class FlatLandGenerator extends ChunkGenerator {
 	   return new ArrayList<BlockPopulator>();	
 	}
 	
-    @SuppressWarnings("deprecation")
-	public byte[][] generatorBlockSections(World world, Random random, 
-    		int chunkX, int ChunkY, BiomeGrid biomeGrid){
-    	byte[][] result = new byte[256 / 16][];
-    	int x,y,z;
-    	
-    	for(x=0; x < 16; x++ ){
-    		for (z = 0; z < 16; z++){
-    		    setBlock(result, x, 0, z, (byte) Material.BEDROCK.getId());   	
-    		}
-    	}
-    	
-    	for(x=0; x < 16; x++ ){
-    		for (z = 0; z < 16; z++){
-    			for(y = 1;y<=2;y++){
-    		    setBlock(result, x, 0, z, (byte) Material.DIRT.getId());   	
-    			}
-    	     }
-    	for(x=0; x < 16; x++ ){
-        	for (z = 0; z < 16; z++){
-        		 setBlock(result, x, 3, z, (byte) Material.GOLD_BLOCK.getId());   	
-        		}
-        	}
-    	}
-    	
-    	return result;
-    }
+    
     @SuppressWarnings("deprecation")
 	public short[][] generatorExtBlockSections(World world, Random random, 
     		int chunkX, int ChunkY, BiomeGrid biomeGrid){
@@ -79,12 +53,6 @@ public class FlatLandGenerator extends ChunkGenerator {
     	}
     	
     	return result;
-    }
-    private void setBlock(byte[][] result, int x, int y, int z, byte blockId) {
-    	if (result[y >> 4] == null){
-    		result[y >> 4] = new byte[4096];
-    	}
-        result[y >> 4][((y&0xF)<<8) | (z >> 4) | x] = blockId;
     }
     private void setBlock(short[][] result, int x, int y, int z, short blockId) {
     	if (result[y >> 4] == null){
